adf5279e00e6501164a94fa8217901c6fa5572f5
수민 차 <smcha@aspnc.com>
2025-04-11 00:44:42 +0000

    Initial commit

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,11 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/format/NumberFormat",
-    "sap/ui/core/Locale",
     "sap/ui/model/Filter"
-], (Controller, NumberFormat, Locale, Filter) => {
+], (Controller, Filter) => {
     "use strict";
-
+Q
     return Controller.extend("fi0011.controller.Main", {
         onInit() {
             this.oRouter = this.getOwnerComponent().getRouter();
@@ -16,181 +14,35 @@ sap.ui.define([
             this.getView().destroyContent();
         },
 
-        currencyFormat: function(sValue, sCurrency) {
-            if (!sValue || !sCurrency) {
-                return "";
-            };
-
-            var oLocale = new Locale("en");
-
-            switch (sCurrency) {
-                case "CNY":
-                    oLocale = new Locale("zh-CN");
-                    break;
-
-                case "USD":
-                    oLocale = new Locale("en-US");
-                    break;
-                
-                case "EUR":
-                    oLocale = new Locale("de-DE");
-                    break;
-                
-                default:
-                    oLocale = new Locale("ko-KR");
-            }
-
-            var oCurrencyFormat = NumberFormat.getCurrencyInstance({
-                currencyCode: true,
-                locale: oLocale
-            });
-            
-            return oCurrencyFormat.format(parseFloat(sValue), sCurrency);
-        },
-
         _onPatternMatched() {
-
+            
         },
 
-        async onSearch(oEvent) {
-            const oTable = this.byId("fi0011.treeTable");
+        onBeforeRebindTable(oEvent) {
+            const oBindingParams = oEvent.getParameter("bindingParams");
 
-            oTable.setBusy(true);
+            let aFilters = oBindingParams.filters || [];
 
-            const oSmartFilterBar = this.byId("fi0011.smartFilterBar");
-            const oFilterData = oSmartFilterBar.getFilterData();
-
-            const sPostingYear = oFilterData.PostingYear;
-            const aSalesOffices = oFilterData.SalesOffice?.items;
-            const aCustomers = oFilterData.Customer?.items;
-
-            let aFilters;
-
-            aFilters = await this._setFilters(sPostingYear, aSalesOffices, aCustomers);
-
-            let aParentNode = new Set();
-            let aParentFilters = [];
-
-            // 레벨 2
-            let aFetchedData = await this._fetchData(aFilters);
-
-            if (aFetchedData.length === 0) {
-                oTable.setBusy(false);
-                return;
-            }
-
-            aFetchedData.forEach(function (oFetchedData) {
-                if (!aParentNode.has(oFetchedData.ParentNodeID)) {
-                    aParentNode.add(oFetchedData.ParentNodeID);
-                    aParentFilters.push(new Filter("NodeID", "EQ", oFetchedData.ParentNodeID));
-                }
-            });
-
-            aFilters = [];
-
-            aFilters.push(new Filter(aParentFilters, false));
-
-            // 레벨 2
-            aParentNode.clear();
-            aParentFilters = [];
-
-            aFetchedData = await this._fetchData(aFilters);
-                                         
-
-            aFetchedData.forEach(function (oFetchedData) {
-                if (!aParentNode.has(oFetchedData.SalesOffice)) {
-                    aParentNode.add(oFetchedData.SalesOffice);
-                    aParentFilters.push(new Filter("SalesOffice", "EQ", oFetchedData.SalesOffice));
-                }
-            });
-
-            aFilters = [];
-
-            aFilters.push(new Filter(aParentFilters, false));
-
-            oTable.bindRows({
-                path: "/ZFI_C_0011_01",
-                filters: aFilters,
-                parameters: {					
-					countMode: "Inline",
-					treeAnnotationProperties:  {
-						hierarchyLevelFor : "HierarchyLevel",
-	                    hierarchyNodeFor : "NodeID",
-	                    hierarchyParentNodeFor : "ParentNodeID",
-	                    hierarchyDrillStateFor : "DrillState"
-					}
-				}
-            });
-
-            this.byId("fi0011.postingYear").setValue(sPostingYear);
-
-            if (aSalesOffices && aSalesOffices.length > 0) {
-                let sValue = "";
-
-                aSalesOffices.forEach(oSalesOffice => {
-                    sValue += oSalesOffice.key + ", ";
-                });
-                
-                sValue = sValue.slice(0, -2);
-                
-                this.byId("fi0011.salesOffice").setValue(sValue);
-            }
-
-            if (aCustomers && aCustomers.length > 0) {
-                let sValue = "";
-
-                aCustomers.forEach(oCustomer => {
-                    sValue += oCustomer.key + ", ";
-                });
-
-                sValue = sValue.slice(0, -2);
-
-                this.byId("fi0011.salesOffice").setValue(sValue);
-            }
-
-            oTable.setBusy(false);
-        },
-
-        _setFilters(sPostingYear, aSalesOffices, aCustomers) {
-            const aFilters = [];
+            const oSelect = this.byId("fi0011.customSelect");
+            const sSelectedKey = oSelect.getSelectedKey();
         
-            if (sPostingYear) {
-                aFilters.push(new Filter("PostingYear", "EQ", sPostingYear));
+            let oNewFilter;
+
+            if (sSelectedKey === "1") {
+                oNewFilter = new Filter("TransactionCurrency", "EQ", "KRW");
+            } else {
+                oNewFilter = new Filter("TransactionCurrency", "NE", "KRW");
             }
         
-            if (aSalesOffices && aSalesOffices.length > 0) {
-                const aSalesOfficeFilters = aSalesOffices.map(oSalesOffice =>
-                    new Filter("SalesOffice", "EQ", oSalesOffice.key)
-                );
-
-                aFilters.push(new Filter(aSalesOfficeFilters, false));
-            }
+            aFilters.push(oNewFilter);
         
-            if (aCustomers && aCustomers.length > 0) {
-                const aCustomerFilters = aCustomers.map(oCustomer =>
-                    new Filter("Customer", "EQ", oCustomer.key)
-                );
+            oBindingParams.filters = aFilters;
 
-                aFilters.push(new Filter(aCustomerFilters, false));
-            }
-        
-            return aFilters;
-        },
-
-        _fetchData(aFilters) {
-            const oModel = this.getView().getModel();
-
-            return new Promise((resolve, reject) => {
-                oModel.read("/ZFI_C_0011_01", {
-                    filters: aFilters || [],
-                    success: function(oReturn) {
-                        resolve(oReturn.results);
-                    },
-                    error: function(oError) {
-                        reject(oError);
-                    }
-                });
-            });
+            oBindingParams.sorter = [
+                new sap.ui.model.Sorter("SalesOffice", false),
+                new sap.ui.model.Sorter("Customer", false),
+                new sap.ui.model.Sorter("OrderNO", false)
+            ];
         }
     });
 });
